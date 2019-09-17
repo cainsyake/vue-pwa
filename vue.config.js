9a98196f03ae5c3d2045453cae2366380128e196
cainsyake <cainsyake@qq.com>
2019-09-17 20:37:40 +0800

    feat(build): 调整PWA配置

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: cainsyake
+ * @Date:   2019-09-17
+ * @Remark:
+ */
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    themeColor: '#2d8cf0',
+    manifestOptions: {
+      start_url: '/?pwa=1'
+    }
+  }
+}
