3de207efa7795db25f6480c0579c1db3af8c6c6a
cainsyake <cainsyake@qq.com>
2019-09-17 20:22:13 +0800

    feat(app): 修改文本

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Vue pwa App"/>
     <div @click="handleCountClick">点击次数：{{count}}</div>
   </div>
 </template>
